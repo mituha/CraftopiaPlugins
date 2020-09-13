750015abb6c13ac153f42df2d3539055b0c6aed4
Mituha Mizuki <mituha@restar.jp>
2020-09-13 13:22:21 +0900

    Hello world!

---
+++ HelloWorldPlugin/HelloWorldPlugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using System;
+
+namespace HelloWorldPlugin
+{
+    [BepInPlugin("me.mituha.craftopia.plugins.helloworldplugin", "Output Hello World Plug-In", "1.0.0.0")]
+    public class HelloWorldPlugin: BaseUnityPlugin
+    {
+        void Awake()
+        {
+            //出力を確認するには、BepInEx.cfg の設定変更が必要
+            //[Logging]
+            //  LogConsoleToUnityLog = true
+            //[Logging.Console]
+            //  Enabled = true
+            UnityEngine.Debug.Log("Hello world!");
+        }
+    }
+}

89452ea871dd72222dbb95635e8c7623456bd136
Mituha Mizuki <mituha@restar.jp>
2020-09-13 13:57:03 +0900

    TestPlugin2追加

---
+++ TestPlugin/TestPlugin2.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using System;
+
+namespace TestPlugin
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// 複数のプラグインクラスは可能か？
+    /// </remarks>
+    [BepInPlugin("me.mituha.craftopia.plugins.testplugin2", "Test Plug-In 2", "1.0.0.0")]
+    public class TestPlugin2: BaseUnityPlugin
+    {
+        void Awake()
+        {
+            //出力を確認するには、BepInEx.cfg の設定変更が必要
+            //[Logging]
+            //  LogConsoleToUnityLog = true
+            //[Logging.Console]
+            //  Enabled = true
+            UnityEngine.Debug.Log($"{this.GetType().Name} Awake");
+        }
+    }
+}

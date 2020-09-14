9ec622d508848f5dbac138f360eaab723cc10763
Mituha Mizuki <mituha@restar.jp>
2020-09-14 20:46:56 +0900

    SingletonMonoBehaviour<T>派生取得コードをTestPluginから削除
    TestUtility側に移行してあります。

--- TestPlugin/TestPlugin.cs
+++ TestPlugin/TestPlugin.cs
@@ -16,35 +16,6 @@ namespace TestPlugin
             //[Logging.Console]
             //  Enabled = true
             UnityEngine.Debug.Log($"{this.GetType().Name} Awake");
-
-            foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
-                UnityEngine.Debug.Log($"{a.GetName().Name}\t:\t{a.FullName}");
-                if (a.GetName().Name == "Assembly-CSharp") {
-                    List<Type> singletonTypes = new List<Type>();
-
-                    //色々検索
-                    foreach (var t in a.GetTypes()) {
-                        UnityEngine.Debug.Log($"\t{t.Name}");
-                        if (t.BaseType == null) { continue; }
-                        //UnityEngine.Debug.Log($"\t\tBaseType:{t.BaseType.Name}");
-
-                        //SingletonMonoBehaviour<T>派生取得
-                        if (t.BaseType.IsGenericType && t.BaseType.GetGenericTypeDefinition() == typeof(SingletonMonoBehaviour<>)) {
-                            // this.Logger.LogInfo($"\t{t.Name}\t:{t.BaseType.Name}");
-                            singletonTypes.Add(t);
-                        }
-                    }
-
-                    if (singletonTypes.Any()) {
-                        //ログファイルにも出力
-                        this.Logger.LogInfo($"SingletonMonoBehaviour<T>派生");
-                        foreach (var t in singletonTypes) {
-                            //ログファイルにも出力
-                            this.Logger.LogInfo($"{t.Name}");
-                        }
-                    }
-                }
-            }
         }
 
         private float _CheckTime = 0;
